921c30955054d6a0dbcc98dd3eb8163a8695a9fc
4074 <137657976@qq.com>
2020-03-02 23:55:31 +0800

    Add BST class

--- test.js
+++ test.js
@@ -1,46 +1,74 @@
-/**
- * Topological sort
- * @param number[][] graph by linked list 
- */
-function topoSort(graph) {
-    const n = graph.length
-
-    // Initial indegree
-    // indegree[i] represent how many pre items the i-th item has 
-    const indegree = Array(n).fill(0)
-    for (let i = 0; i < n; i += 1) {
-        for (let j = 0; j < graph[i].length; j += 1) {
-            indegree[graph[i][j]] += 1
-        }
+class BST {
+    constructor() {
+        this.root = null
     }
 
-    const sorted = []
-    // Push all items in sorted
-    while (sorted.length < n) {
+    add(val) {
+        const current = new BSTNode(val)
+        let parent = null
+        let node = this.root
 
-        // Find a item with zero indegree
-        let j = 0
-        while (j < n && indegree[j] != 0) {
-            j += 1
+        while (node) {
+            parent = node
+            if (val < node.val) {
+                node = node.left
+            } else {
+                node = node.right
+            }
         }
 
-        // If no such item, the graph is cyclic, return empty array
-        if (j === n) return []
-
-        // Push item in sorted
-        // And set indegree to -1, mark it pushed
-        indegree[j] -= 1
-        sorted.push(j)
-
-        // Minus 1 the indegree of the items next to this item 
-        for (let k = 0; k < graph[j].length; k += 1) {
-            indegree[graph[j][k]] -= 1
+        if (parent) {
+            if (val < parent.val) {
+                parent.left = current
+            } else {
+                parent.right = current
+            }
+        } else {
+            this.root = current
         }
+
+        return this
     }
 
-    return sorted
+    remove(val) {
+        
+    }
+
+    min() {
+        let node = this.root
+        if (!node) return
+        while (node.left) {
+            node = node.left
+        }
+        return node.val
+    }
+
+    max() {
+        let node = this.root
+        if (!node) return
+        while (node.right) {
+            node = node.right
+        }
+        return node.val
+    }
+
+    inorder() {}
+
+    preorder() {}
+
+    postorder() {}
 }
 
-console.log(topoSort([
-    [], [0], [1], [2]
-]))
+class BSTNode {
+    constructor(val) {
+        this.val = val
+        this.left = null
+        this.right = null
+    }
+}
+
+const tree = new BST()
+tree.add(3).add(1).add(2).add(4)
+console.log(tree)
+console.log(tree.min())
+console.log(tree.max())
