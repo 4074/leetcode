0fcd1517ada3d288d84b90659c4ec7e571f39bc0
4074 <137657976@qq.com>
2020-03-05 00:31:17 +0800

    Add remove method for BST class

--- test.js
+++ test.js
@@ -31,16 +31,74 @@ class BST {
     }
 
     remove(val) {
-        
+        const current = this.fincChild(this.root, val)
+        if (current) {
+            let { parent, node } = current
+            let child = null
+            if (!node.left) {
+                child = node.right
+            } else if (!node.right) {
+                child = node.left
+            } else {
+                const minChildOfRight = this.findMinChild(node.right)
+                if (node === minChildOfRight.parent) {
+                    child = minChildOfRight.node
+                } else {
+                    minChildOfRight.parent.left = minChildOfRight.node.right
+                    child = minChildOfRight.node
+                    child.right = node.right
+                }
+                child.left = node.left
+            }
+
+            if (parent) {
+                parent[parent.val < node.val ? 'right' : 'left'] = child
+            } else {
+                this.root = child
+            }
+        }
     }
 
     min() {
-        let node = this.root
-        if (!node) return
+        const minChild = this.findMinChild(this.root)
+        return minChild && minChild.node && minChild.node.val
+    }
+
+    has(val) {
+        return !!this.fincChild(this.root, val)
+    }
+
+    fincChild(root, val) {
+        let parent = null
+        let node = root
+        while (node) {
+            if (node.val === val) {
+                return {
+                    parent,
+                    node
+                }
+            }
+            parent = node
+            if (val < node.val) {
+                node = node.left
+            } else {
+                node = node.right
+            }
+        }
+    }
+
+    findMinChild(root) {
+        if (!root) return
+        let parent = null
+        let node = root
         while (node.left) {
+            parent = node
             node = node.left
         }
-        return node.val
+        return {
+            parent,
+            node
+        }
     }
 
     max() {
@@ -68,7 +126,9 @@ class BSTNode {
 }
 
 const tree = new BST()
-tree.add(3).add(1).add(2).add(4)
+tree.add(5).add(4).add(8).add(6).add(7)
 console.log(tree)
 console.log(tree.min())
 console.log(tree.max())
+tree.remove(5)
+console.log(tree)
