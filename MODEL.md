5a4b92f1521f2ed50c661cd3f403167b40d8248d
4074 <137657976@qq.com>
2020-03-07 21:10:31 +0800

    Finish Binary Search Tree

--- MODEL.md
+++ MODEL.md
@@ -47,3 +47,191 @@ function topoSort(graph) {
 }
 topoSort([[], [0], [1], [2]]) // [3, 2, 1, 0]
 ```
+
+## Tree
+
+### Binary Search Tree
+```javascript
+class BinarySearchTree {
+    constructor() {
+        this.root = null
+    }
+
+    insert(val) {
+        this.root = this.insertNode(this.root, val)
+        return this
+    }
+
+    delete(val) {
+        this.root = this.deleteNode(this.root, val)
+        return this
+    }
+
+    min() {
+        const minChild = this.findMinChild(this.root)
+        return minChild && minChild.node && minChild.node.val
+    }
+
+    max() {
+        if (this.root) return
+        let node = this.root
+        while (node.right) {
+            node = node.right
+        }
+        return node.val
+    }
+
+    has(val) {
+        return !!this.fincChild(this.root, val)
+    }
+
+    insertNode(root, val) {
+        const node = new Node(val)
+        if (!root) return node
+
+        // Find the null node with val
+        // Parent will store the null node's parent
+        let parent = null
+        let current = this.root
+
+        while (current) {
+            parent = current
+            if (node.val <= current.val) {
+                current = current.left
+            } else {
+                current = current.right
+            }
+        }
+
+        // Insert the node into the tree.
+        if (node.val < parent.val) {
+            parent.left = node
+        } else {
+            parent.right = node
+        }
+
+        return root
+    }
+
+    deleteNode(root, val) {
+        const current = this.fincChild(root, val)
+        if (!current) return
+        
+        let { parent, node } = current
+        let child = null
+        
+        if (!node.left) {
+            // 1. No left child, the right child take the current place.
+            child = node.right
+        } else if (!node.right) {
+            // 2. No right child, the left child take the current place.
+            child = node.left
+        } else {
+            // 3. Has two children, find the min node of right child.
+            // Remove it from tree, and then insert it into current place.
+            const minChildOfRight = this.findMinChild(node.right)
+
+            if (minChildOfRight.node === node.right) {
+                // 3.1 If the min node is current's right child, the same as 1
+                child = minChildOfRight.node
+            } else {
+                // The min child has no left child, and has right child.
+                // So remove it using a shorthand impletement, but not use this.remove
+                minChildOfRight.parent.left = minChildOfRight.node.right
+                child = minChildOfRight.node
+                child.right = node.right
+            }
+
+            // Link current left child to the new current
+            child.left = node.left
+        }
+
+        // If no parent, the deleted node is root.
+        // Let the child be the new root
+        if (!parent) return child
+
+        // Link the new current node to the parent
+        parent[parent.val < node.val ? 'right' : 'left'] = child
+
+        return root
+    }
+
+    fincChild(root, val) {
+        let parent = null
+        let node = root
+        while (node) {
+            if (node.val === val) {
+                return {
+                    parent,
+                    node
+                }
+            }
+            parent = node
+            if (val < node.val) {
+                node = node.left
+            } else {
+                node = node.right
+            }
+        }
+    }
+
+    findMinChild(root) {
+        if (!root) return
+        let parent = null
+        let node = root
+        while (node.left) {
+            parent = node
+            node = node.left
+        }
+        return {
+            parent,
+            node
+        }
+    }
+
+    preorder() {
+        return this.walk(this.root, 'pre', [])
+    }
+
+    inorder() {
+        return this.walk(this.root, 'in', [])
+    }
+
+    postorder() {
+        return this.walk(this.root, 'post', [])
+    }
+
+    walk(node, order, result) {
+        if (!node) return result
+        if (order === 'pre') {
+            result.push(node.val)
+            this.walk(node.left, order, result)
+            this.walk(node.right, order, result)
+        } else if (order === 'in') {
+            this.walk(node.left, order, result)
+            result.push(node.val)
+            this.walk(node.right, order, result)
+        } else {
+            this.walk(node.left, order, result)
+            this.walk(node.right, order, result)
+            result.push(node.val)
+        }
+        return result
+    }
+}
+
+class Node {
+    constructor(val) {
+        this.val = val
+        this.left = null
+        this.right = null
+    }
+}
+
+const tree = new BinarySearchTree()
+tree.insert(5).insert(2).insert(4).insert(3).insert(6).insert(7)
+console.log(tree)
+console.log(tree.preorder())
+console.log(tree.inorder())
+console.log(tree.postorder())
+```
--- test.js
+++ test.js
@@ -3,78 +3,14 @@ class BinarySearchTree {
         this.root = null
     }
 
-    add(val) {
-        // Return the val exists
-        if (this.has(val)) return
-
-        // Find the null node with val
-        // Parent will store the null node's parent
-        const current = new Node(val)
-        let parent = null
-        let node = this.root
-
-        while (node) {
-            parent = node
-            if (val < node.val) {
-                node = node.left
-            } else {
-                node = node.right
-            }
-        }
-
-        // Insert the node into the tree, or be the root.
-        if (parent) {
-            if (val < parent.val) {
-                parent.left = current
-            } else {
-                parent.right = current
-            }
-        } else {
-            this.root = current
-        }
-
+    insert(val) {
+        this.root = this.insertNode(this.root, val)
         return this
     }
 
-    remove(val) {
-        const current = this.fincChild(this.root, val)
-        if (current) {
-            let { parent, node } = current
-            let child = null
-            
-            if (!node.left) {
-                // 1. No left child, the right child take the current place.
-                child = node.right
-            } else if (!node.right) {
-                // 2. No right child, the left child take the current place.
-                child = node.left
-            } else {
-                // 3. Has two children, find the min node of right child.
-                // Remove it from tree, and then insert it into current place.
-                const minChildOfRight = this.findMinChild(node.right)
-
-                if (minChildOfRight.node === node.right) {
-                    // 3.1 If the min node is current's right child, the same as 1
-                    child = minChildOfRight.node
-                } else {
-                    // The min child has no left child, and has right child.
-                    // So remove it using a shorthand impletement, but not use this.remove
-                    minChildOfRight.parent.left = minChildOfRight.node.right
-                    child = minChildOfRight.node
-                    child.right = node.right
-                }
-
-                // Link current left child to the new current
-                child.left = node.left
-            }
-
-            // Link the new current node to the parent
-            if (parent) {
-                parent[parent.val < node.val ? 'right' : 'left'] = child
-            } else {
-                this.root = child
-            }
-        }
+    delete(val) {
+        this.root = this.deleteNode(this.root, val)
+        return this
     }
 
     min() {
@@ -83,8 +19,8 @@ class BinarySearchTree {
     }
 
     max() {
+        if (this.root) return
         let node = this.root
-        if (!node) return
         while (node.right) {
             node = node.right
         }
@@ -93,6 +29,77 @@ class BinarySearchTree {
 
     has(val) {
         return !!this.fincChild(this.root, val)
+    }
+
+    insertNode(root, val) {
+        const node = new Node(val)
+        if (!root) return node
+
+        // Find the null node with val
+        // Parent will store the null node's parent
+        let parent = null
+        let current = this.root
+
+        while (current) {
+            parent = current
+            if (node.val <= current.val) {
+                current = current.left
+            } else {
+                current = current.right
+            }
+        }
+
+        // Insert the node into the tree.
+        if (node.val < parent.val) {
+            parent.left = node
+        } else {
+            parent.right = node
+        }
+
+        return root
+    }
+
+    deleteNode(root, val) {
+        const current = this.fincChild(root, val)
+        if (!current) return
+        
+        let { parent, node } = current
+        let child = null
+        
+        if (!node.left) {
+            // 1. No left child, the right child take the current place.
+            child = node.right
+        } else if (!node.right) {
+            // 2. No right child, the left child take the current place.
+            child = node.left
+        } else {
+            // 3. Has two children, find the min node of right child.
+            // Remove it from tree, and then insert it into current place.
+            const minChildOfRight = this.findMinChild(node.right)
+
+            if (minChildOfRight.node === node.right) {
+                // 3.1 If the min node is current's right child, the same as 1
+                child = minChildOfRight.node
+            } else {
+                // The min child has no left child, and has right child.
+                // So remove it using a shorthand impletement, but not use this.remove
+                minChildOfRight.parent.left = minChildOfRight.node.right
+                child = minChildOfRight.node
+                child.right = node.right
+            }
+
+            // Link current left child to the new current
+            child.left = node.left
+        }
+
+        // If no parent, the deleted node is root.
+        // Let the child be the new root
+        if (!parent) return child
+
+        // Link the new current node to the parent
+        parent[parent.val < node.val ? 'right' : 'left'] = child
+
+        return root
     }
 
     fincChild(root, val) {
@@ -128,11 +135,35 @@ class BinarySearchTree {
         }
     }
 
-    inorder() {}
+    preorder() {
+        return this.walk(this.root, 'pre', [])
+    }
 
-    preorder() {}
+    inorder() {
+        return this.walk(this.root, 'in', [])
+    }
 
-    postorder() {}
+    postorder() {
+        return this.walk(this.root, 'post', [])
+    }
+
+    walk(node, order, result) {
+        if (!node) return result
+        if (order === 'pre') {
+            result.push(node.val)
+            this.walk(node.left, order, result)
+            this.walk(node.right, order, result)
+        } else if (order === 'in') {
+            this.walk(node.left, order, result)
+            result.push(node.val)
+            this.walk(node.right, order, result)
+        } else {
+            this.walk(node.left, order, result)
+            this.walk(node.right, order, result)
+            result.push(node.val)
+        }
+        return result
+    }
 }
 
 class Node {
@@ -144,11 +175,8 @@ class Node {
 }
 
 const tree = new BinarySearchTree()
-tree.add(1).add(2).add(0)
+tree.insert(5).insert(2).insert(4).insert(3).insert(6).insert(7)
 console.log(tree)
-console.log(tree.min())
-console.log(tree.max())
-tree.remove(1)
-console.log(tree)
-tree.remove(0)
-console.log(tree)
+console.log(tree.preorder())
+console.log(tree.inorder())
+console.log(tree.postorder())
