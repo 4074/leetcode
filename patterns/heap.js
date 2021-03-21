6ec345270c7b8dfdae058447423d34ba9364a76f
小峰 <dwfn3398@corp.netease.com>
2021-03-21 15:37:01 +0800

    Update heap

--- patterns/heap.js
+++ patterns/heap.js
@@ -2,70 +2,51 @@ class Heap {
   arr = []
   size = 0
 
-  constructor(arr, isMinHeap, compare) {
-    this.arr = arr || []
-    this.size = this.arr.length
-    this.compare = compare || this.compare(isMinHeap)
-
-    // Build the heap.
-    // Call the heapify by bottom-up.
-    for (let i = Math.floor((this.size - 1) / 2); i >= 0; i -= 1) {
-      this.heapify(i)
-    }
-  }
-
-  compare(isMinHeap) {
-    return (parent, child) => {
-      if (parent === child) return -1
-      return (parent > child ? -1 : 1) * (isMinHeap ? -1 : 1)
-    }
-  }
-
-  getLeftIndex(index) {
-    return index * 2 + 1
-  }
-
-  getRightIndex(index) {
-    return index * 2 + 2
-  }
-
-  getParentIndex(index) {
-    return Math.floor((index - 1) / 2)
-  }
-
-  swap(i, j) {
+  left = index => index * 2 + 1
+  right = index => index * 2 + 2
+  parent = index => Math.floor((index - 1) / 2)
+  swap = (i, j) => {
     const tmp = this.arr[i]
     this.arr[i] = this.arr[j]
     this.arr[j] = tmp
   }
 
+  constructor(arr, comparator) {
+    this.arr = arr || []
+    this.comparator = comparator || ((parent, child) => parent < child ? -1 : 1)
+    this.size = this.arr.length
+
+    for (let i = Math.floor((this.size - 1) / 2); i >= 0; i -= 1) {
+      this.heapify(i)
+    }
+  }
+
   heapify(index) {
-    let largestIndex = index
+    let largest = index
+    const l = this.left(index)
+    const r = this.right(index)
 
-    const leftIndex = this.getLeftIndex(index)
-    const rightIndex = this.getRightIndex(index)
+    if (l < this.size && this.comparator(this.arr[largest], this.arr[l]) > 0)
+      largest = l
+    if (r < this.size && this.comparator(this.arr[largest], this.arr[r]) > 0)
+      largest = r
 
-    if (leftIndex < this.size && this.compare(this.arr[largestIndex], this.arr[leftIndex]) > 0)
-      largestIndex = leftIndex
-
-    if (rightIndex < this.size && this.compare(this.arr[largestIndex], this.arr[rightIndex]) > 0)
-      largestIndex = rightIndex
-
-    if (index === largestIndex) return
-    this.swap(index, largestIndex)
-    this.heapify(largestIndex)
+    if (index !== largest) {
+      this.swap(index, largest)
+      this.heapify(largest)
+    }
   }
 
   push(item) {
     this.arr.push(item)
     this.size += 1
-    let childIndex = this.size - 1
-    let parentIndex = this.getParentIndex(childIndex)
 
-    while (parentIndex >= 0 && this.compare(this.arr[parentIndex], this.arr[childIndex]) > 0) {
-      this.swap(childIndex, parentIndex)
-      childIndex = parentIndex
-      parentIndex = this.getParentIndex(childIndex)
+    let c = this.size - 1
+    let p = this.parent(c)
+    while (p >= 0 && this.comparator(this.arr[p], this.arr[c]) > 0) {
+      this.swap(c, p)
+      c = p
+      p = this.parent(p)
     }
 
     return this
@@ -79,6 +60,10 @@ class Heap {
     return this.arr.pop()
   }
 
+  peek() {
+    return this.arr[0]
+  }
+
   sort() {
     while (this.size > 1) {
       this.swap(0, this.size - 1)
@@ -86,6 +71,7 @@ class Heap {
       this.heapify(0)
     }
     this.size = this.arr.length
+    return [...this.arr]
   }
 }
 
@@ -97,7 +83,7 @@ console.log(heap.arr)
 function testSort() {
   const randomArr = Array(1000).fill().map(() => Math.round(Math.random() * 10000))
   const randomArrSorted = [...randomArr].sort((a, b) => b - a)
-  const sortHeap = new Heap(randomArr, true)
+  const sortHeap = new Heap(randomArr)
   sortHeap.sort()
   // console.log(sortHeap.arr)
   // console.log(randomArrSorted)
@@ -109,6 +95,6 @@ for (let i = 0; i < 10; i += 1) {
 }
 
 // Test push/pop
-const minHeap = new Heap([], true)
+const minHeap = new Heap([])
 minHeap.push(8).push(5).push(15)
 console.log(minHeap.pop())
